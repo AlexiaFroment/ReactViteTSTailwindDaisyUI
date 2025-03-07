c60b38bdec1224fab40570c5e8ffa27e1d4e841f
AloaCmoi <aloacmoi@gmail.com>
2025-03-07 11:03:19 +0100

    Fist commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl text-amber-500'>Hello world</h1>
+      <br />
+      <button className='inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900'>
+        Button
+      </button>
+      <button className='btn btn-primary'>Button</button>
+    </>
+  )
+}
+
+export default App
